b7cb75e4baf7c84786e4d67b737e0a1e7438bae0
Ids Klijnsma <ids.klijnsma@clockwork.nl>
2015-10-01 10:37:17 +0200

    Added month and day replace

--- date-replace.js
+++ date-replace.js
@@ -1,24 +1,4 @@
-require('')
-
-function mmlongReplace(dstring) {
-    var localMonths = [];
-    localMonths.Jan = 'januari';
-    localMonths.Feb = 'februari';
-    localMonths.Mar = 'maart';
-    localMonths.Apr = 'april';
-    localMonths.May = 'mei';
-    localMonths.Jun = 'juni';
-    localMonths.Jul = 'juli';
-    localMonths.Aug = 'augustus';
-    localMonths.Sep = 'september';
-    localMonths.Oct = 'oktober';
-    localMonths.Nov = 'november';
-    localMonths.Dec = 'december';
-    for (var lm in localMonths) {
-        dstring = dstring.replace(lm, localMonths[lm]);
-    }
-    return dstring;
-}
+var fs = require('');
 
 /**
  * @name replaceDates
@@ -29,50 +9,92 @@ function mmlongReplace(dstring) {
  * Used for test data, replaces date functions with the required date in a certain format
  * You can also subtract weeks/months/years
  */
-function dateReplace(match) {
-    var cleanedMatch = match.replace('${date(','').replace(')}',''),
-        props = cleanedMatch.split(','),
-        today = Date.today(),
+function dateReplace() {
+    var lang = 'nl',
+        l10n;
 
-        dateFormat,
-        dateAdd,
-        dateReturn,
-        addObject = { days: 0,
-            months: 0,
-            years: 0},
-        mmmlong = false;
+    return {
+        setL10n: setL10n,
+        replace: replace
+    };
 
-    if (props.length === 2) {
-        dateAdd = props[0].replace(/"/g,'');
-        dateFormat = props[1].replace(/"/g,'').replace(' ','').toUpperCase();
-        if (dateAdd.indexOf('d')>0) {
-            dateAdd = parseInt(dateAdd.replace('d',''));
-            addObject.days = dateAdd;
-        } else if (dateAdd.indexOf('m')>0) {
-            dateAdd = parseInt(dateAdd.replace('m',''));
-            addObject.months = dateAdd;
-        } else if (dateAdd.indexOf('y')>0) {
-            dateAdd = parseInt(dateAdd.replace('y',''));
-            addObject.years = dateAdd;
+    function setL10n(newLang) {
+        try{
+            l10n = fs.readFileSync('l10n/' + newLang + '.json', 'utf8');
+            l10n = JSON.parse(l10n);
+            lang = newLang;
+        }catch(e) {
+            throw "Language not found";
+        }
+    }
+
+    function l10nReplace(match) {
+        match = match.replace(/<<month:(\d+)>>/g, function(match, month) {
+            return l10n.months[month];
+        });
+
+        match = match.replace(/<<day:(\d+)>>/g, function(match, day) {
+            return l10n.days[day];
+        });
+
+        return match;
+    }
+
+    function replace(match) {
+        if(!l10n) {
+            setL10n(lang);
         }
 
-        today.add(addObject);
-    } else {
-        dateAdd = false;
-        dateFormat = props[0].replace(/"/g,'').replace(' ','').toUpperCase();
+        var cleanedMatch = match.replace('${date(', '').replace(')}', ''),
+            props = cleanedMatch.split(','),
+            today = Date.today(),
+
+            dateFormat,
+            dateAdd,
+            dateReturn,
+            addObject = {
+                days: 0,
+                months: 0,
+                years: 0
+            },
+            mmmlong = false;
+
+        if (props.length === 2) {
+            dateAdd = props[0].replace(/"/g, '');
+            dateFormat = props[1].replace(/"/g, '').replace(' ', '').toUpperCase();
+
+            if (dateAdd.indexOf('d') > 0) {
+                dateAdd = parseInt(dateAdd.replace('d', ''));
+                addObject.days = dateAdd;
+
+            } else if (dateAdd.indexOf('m') > 0) {
+                dateAdd = parseInt(dateAdd.replace('m', ''));
+                addObject.months = dateAdd;
+
+            } else if (dateAdd.indexOf('y') > 0) {
+                dateAdd = parseInt(dateAdd.replace('y', ''));
+                addObject.years = dateAdd;
+            }
+
+            today.add(addObject);
+
+        } else {
+            dateFormat = props[0].replace(/"/g, '').replace(' ', '').toUpperCase();
+        }
+
+        if (dateFormat.indexOf('MMMLONG') > -1) {
+            mmmlong = true;
+            dateFormat = dateFormat.replace('MMMLONG', '<<month:M>>');
+        }
+
+        dateReturn = today.toFormat(dateFormat);
+        if (mmmlong) {
+            dateReturn = l10nReplace(dateReturn);
+        }
+        return dateReturn;
     }
 
-    // Parse MMMLONG format: Dutch Long Months:
-    if (dateFormat.indexOf('MMMLONG') > -1) {
-        mmmlong = true;
-        dateFormat = dateFormat.replace('MMMLONG', 'MMM');
-    }
 
-    dateReturn = today.toFormat(dateFormat);
-    if (mmmlong) {
-        dateReturn = mmlongReplace(dateReturn);
-    }
-    return dateReturn;
 }
 
 module.exports = dateReplace;
